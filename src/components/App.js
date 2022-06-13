4c444d653ff021df574e782c15011aff956fb501
Md Mumtaj Alam <98735031+mumtajalam@users.noreply.github.com>
2022-06-13 20:23:04 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,35 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { Component, useState } from "react";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      posi: 0,
+      ballPosition: { left: "0px" }
+    };
+    this.renderChoice = this.renderBallorButton.Bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.Bind(this);
+  }
+
+  buttonClickHandler() {}
+
+  renderBallorButton() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else {
+      return (
+        <button onClick={this.buttonClickHandler}>Click for one ball</button>
+      );
+    }
+  }
+
+  componentDidMount() {}
+  render() {
+    return;
+    <>
+      <div className="playground">{this.renderBallorButton()}</div>
+    </>;
+  }
+}
+export default App;
